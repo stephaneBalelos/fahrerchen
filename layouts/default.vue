6cf0f5c2005232bd1e39694816ea5b7394232152
Stephane <sd@intelligentmobiles.com>
2024-02-20 23:01:07 +0100

    Added nuxtui

---
+++ layouts/default.vue
@@ -0,0 +1,42 @@
+<template>
+    <UMain>
+        <UHeader :links="links">
+            <template #logo>
+                <Logo class="w-auto h-6" />
+            </template>
+
+            <template #right>
+                <UDocsSearchButton label="" />
+
+                <UColorModeButton />
+
+                <UButton to="https://github.com/nuxt/ui" target="_blank" icon="i-simple-icons-github" color="gray"
+                    variant="ghost" />
+            </template>
+
+            <template #panel>
+                Panel
+            </template>
+        </UHeader>
+        <slot></slot>
+    </UMain>
+</template>
+
+<script setup lang="ts">
+
+const links = [{
+  label: 'Docs',
+  icon: 'i-heroicons-book-open',
+  to: '/getting-started'
+}, {
+  label: 'Pro',
+  icon: 'i-heroicons-square-3-stack-3d',
+  to: '/pro'
+}, {
+  label: 'Releases',
+  icon: 'i-heroicons-rocket-launch',
+  to: '/releases'
+}]
+</script>
+
+<style scoped></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   routeRules: {
     // prerender index route by default
     '/': { prerender: true },
   },
+
+  modules: ['@nuxtjs/supabase', '@nuxt/ui'],
+  extends: ['@nuxt/ui-pro'],
+  supabase: {
+    redirect: false
+  }
 });
