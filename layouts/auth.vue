8e8bc4fd5daa86e82e03fed2799f43dced15e511
Stephane <sd@intelligentmobiles.com>
2024-02-20 23:41:32 +0100

    Added auth page base

---
+++ layouts/auth.vue
@@ -0,0 +1,29 @@
+<template>
+    <UMain>
+        <UHeader>
+            <template #logo>
+                <Logo class="w-auto h-6" />
+            </template>
+
+            <template #right>
+                <UDocsSearchButton label="" />
+
+                <UColorModeButton />
+
+                <UButton to="https://github.com/nuxt/ui" target="_blank" icon="i-simple-icons-github" color="gray"
+                    variant="ghost" />
+            </template>
+        </UHeader>
+        <UContainer :ui="{ base: 'flex justify-center items-center' }">
+            <slot></slot>
+        </UContainer>
+    </UMain>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
