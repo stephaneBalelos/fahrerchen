a9b6ad8a07266f53dadec6fd6c590e05823a2c84
Stephane <sd@intelligentmobiles.com>
2024-06-21 15:23:48 +0200

    Fixed hydration error

--- layouts/orgs.vue
+++ layouts/orgs.vue
@@ -1,155 +1,192 @@
 <template>
-    <UMain>
-        <UDashboardLayout>
-            <UDashboardPanel :width="250" :resizable="{ min: 200, max: 300 }" collapsible>
-                <UDashboardNavbar class="!border-transparent" :ui="{ left: 'flex-1' }">
-                    <template #left>
-                        <div class="org-dropdown">
-                            {{ orgData?.name }}
-                        </div>
-                    </template>
-                </UDashboardNavbar>
+  <UMain>
+    <UDashboardLayout>
+      <UDashboardPanel
+        :width="250"
+        :resizable="{ min: 200, max: 300 }"
+        collapsible
+      >
+        <UDashboardNavbar class="!border-transparent" :ui="{ left: 'flex-1' }">
+          <template #left>
+            <div class="org-dropdown">
+              {{ orgData?.name }}
+            </div>
+          </template>
+        </UDashboardNavbar>
 
-                <UDashboardSidebar>
-                    <template #header>
-                        <UDashboardSearchButton />
-                    </template>
+        <UDashboardSidebar>
+          <template #header>
+            <UDashboardSearchButton />
+          </template>
 
-                    <UDashboardSidebarLinks :links="links" />
+          <UDashboardSidebarLinks :links="links" />
 
-                    <UDivider />
+          <UDivider />
 
-                    <UDashboardSidebarLinks :links="[{ label: 'Colors', draggable: false, children: colors }]"
-                        @update:links="links => defaultColors = links" />
+          <UDashboardSidebarLinks
+            :links="[{ label: 'Colors', draggable: false, children: colors }]"
+            @update:links="(links) => (defaultColors = links)"
+          />
 
-                    <div class="flex-1" />
+          <div class="flex-1" />
 
+          <UDivider class="sticky bottom-0" />
 
-                    <UDivider class="sticky bottom-0" />
+          <template #footer>
+            <UDashboardSidebarLinks :links="footerLinks" />
+          </template>
+        </UDashboardSidebar>
+      </UDashboardPanel>
+      <UDashboardPage>
+        <UDashboardPanel grow>
+          <UDashboardNavbar title="Home">
+            <template #right>
+              <UTooltip text="Notifications" :shortcuts="['N']">
+                <UButton color="gray" variant="ghost" square>
+                  <UChip color="red" inset>
+                    <UIcon name="i-heroicons-bell" class="w-5 h-5" />
+                  </UChip>
+                </UButton>
+              </UTooltip>
 
-                    <template #footer>
-                        <UDashboardSidebarLinks :links="footerLinks" />
-                    </template>
-                </UDashboardSidebar>
-            </UDashboardPanel>
-            <UDashboardPage>
-                <UDashboardPanel grow>
-                    <UDashboardNavbar title="Home">
-                        <template #right>
-                            <UTooltip text="Notifications" :shortcuts="['N']">
-                                <UButton color="gray" variant="ghost" square>
-                                    <UChip color="red" inset>
-                                        <UIcon name="i-heroicons-bell" class="w-5 h-5" />
-                                    </UChip>
-                                </UButton>
-                            </UTooltip>
+              <UserDropdown></UserDropdown>
+            </template>
+          </UDashboardNavbar>
 
-                            <UserDropdown></UserDropdown>
-                        </template>
-                    </UDashboardNavbar>
-
-                    <slot></slot>
-                </UDashboardPanel>
-            </UDashboardPage>
-        </UDashboardLayout>
-    </UMain>
+          <ClientOnly>
+            <slot></slot>
+          </ClientOnly>
+        </UDashboardPanel>
+      </UDashboardPage>
+    </UDashboardLayout>
+  </UMain>
 </template>
 
 <script setup lang="ts">
-import UserDropdown from '~/components/navigation/UserDropdown.vue';
-import { useGlobalOrgState } from '~/composables/useOrgScope';
+import UserDropdown from "~/components/navigation/UserDropdown.vue";
+import { useGlobalOrgState } from "~/composables/useOrgScope";
 
-const route = useRoute()
-const appConfig = useAppConfig()
-const { isHelpSlideoverOpen } = useDashboard()
+const route = useRoute();
+const appConfig = useAppConfig();
+const { isHelpSlideoverOpen } = useDashboard();
 
-const user = useSupabaseUser()
-const supabase = useSupabaseClient()
-const toast = useToast()
+const user = useSupabaseUser();
+const supabase = useSupabaseClient();
+const toast = useToast();
 
-const { org, orgData } = useGlobalOrgState()
+const { org, orgData } = useGlobalOrgState();
 
-
-
-const links = [{
-    id: 'dashboard',
-    label: 'Dashboard',
-    icon: 'i-heroicons-view-dashboard',
-    to: '/my',
+const links = [
+  {
+    id: "dashboard",
+    label: "Dashboard",
+    icon: "i-heroicons-view-dashboard",
+    to: "/my",
     tooltip: {
-        text: 'Dashboard',
-        shortcuts: ['G', 'D']
-    }
-}, {
-    id: 'users',
-    label: 'Users',
-    icon: 'i-heroicons-user-group',
-    to: '/my/users',
+      text: "Dashboard",
+      shortcuts: ["G", "D"],
+    },
+  },
+  {
+    id: "users",
+    label: "Users",
+    icon: "i-heroicons-user-group",
+    to: "/my/users",
     tooltip: {
-        text: 'Users',
-        shortcuts: ['G', 'U']
-    }
-}, {
-    id: 'courses',
-    label: 'Courses',
-    icon: 'i-heroicons-book-open',
-    to: '/my/courses',
+      text: "Users",
+      shortcuts: ["G", "U"],
+    },
+  },
+  {
+    id: "courses",
+    label: "Courses",
+    icon: "i-heroicons-book-open",
+    to: "/my/courses",
     tooltip: {
-        text: 'Courses',
-        shortcuts: ['G', 'C']
-    }
-}, {
-    id: 'settings',
-    label: 'Settings',
-    to: '/my/settings',
-    icon: 'i-heroicons-cog-8-tooth',
-    children: [{
-        label: 'General',
-        to: '/my/settings',
-        exact: true
-    }, {
-        label: 'Members',
-        to: '/my/settings/members'
-    }, {
-        label: 'Notifications',
-        to: '/my/settings/notifications'
-    }],
+      text: "Courses",
+      shortcuts: ["G", "C"],
+    },
+  },
+  {
+    id: "settings",
+    label: "Settings",
+    to: "/my/settings",
+    icon: "i-heroicons-cog-8-tooth",
+    children: [
+      {
+        label: "General",
+        to: "/my/settings",
+        exact: true,
+      },
+      {
+        label: "Members",
+        to: "/my/settings/members",
+      },
+      {
+        label: "Notifications",
+        to: "/my/settings/notifications",
+      },
+    ],
     tooltip: {
-        text: 'Settings',
-        shortcuts: ['G', 'S']
-    }
-}]
+      text: "Settings",
+      shortcuts: ["G", "S"],
+    },
+  },
+];
 
-const footerLinks = [{
-    label: 'Invite people',
-    icon: 'i-heroicons-plus',
-    to: '/settings/members'
-}, {
-    label: 'Help & Support',
-    icon: 'i-heroicons-question-mark-circle',
-    click: () => isHelpSlideoverOpen.value = true
-}]
+const footerLinks = [
+  {
+    label: "Invite people",
+    icon: "i-heroicons-plus",
+    to: "/settings/members",
+  },
+  {
+    label: "Help & Support",
+    icon: "i-heroicons-question-mark-circle",
+    click: () => (isHelpSlideoverOpen.value = true),
+  },
+];
 
-const groups = [{
-    key: 'links',
-    label: 'Go to',
-    commands: links.map(link => ({ ...link, shortcuts: link.tooltip?.shortcuts }))
-}, {
-    key: 'code',
-    label: 'Code',
-    commands: [{
-        id: 'source',
-        label: 'View page source',
-        icon: 'i-simple-icons-github',
+const groups = [
+  {
+    key: "links",
+    label: "Go to",
+    commands: links.map((link) => ({
+      ...link,
+      shortcuts: link.tooltip?.shortcuts,
+    })),
+  },
+  {
+    key: "code",
+    label: "Code",
+    commands: [
+      {
+        id: "source",
+        label: "View page source",
+        icon: "i-simple-icons-github",
         click: () => {
-            window.open(`https://github.com/nuxt-ui-pro/dashboard/blob/main/pages${route.path === '/' ? '/index' : route.path}.vue`, '_blank')
-        }
-    }]
-}]
+          window.open(
+            `https://github.com/nuxt-ui-pro/dashboard/blob/main/pages${
+              route.path === "/" ? "/index" : route.path
+            }.vue`,
+            "_blank"
+          );
+        },
+      },
+    ],
+  },
+];
 
-const defaultColors = ref(['green', 'teal', 'cyan', 'sky', 'blue', 'indigo', 'violet'].map(color => ({ label: color, chip: color, click: () => console.log(color) })))
-const colors = computed(() => defaultColors.value.map(color => ({ ...color, active: false })))
-
+const defaultColors = ref(
+  ["green", "teal", "cyan", "sky", "blue", "indigo", "violet"].map((color) => ({
+    label: color,
+    chip: color,
+    click: () => console.log(color),
+  }))
+);
+const colors = computed(() =>
+  defaultColors.value.map((color) => ({ ...color, active: false }))
+);
 </script>
 
 <style scoped></style>
