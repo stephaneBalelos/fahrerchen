f3526f66efecac656e80796039f7245d14e08ecf
stephaneBalelos <stephanedondyas2@gmail.com>
2024-12-23 21:05:17 +0100

    stripe student global

--- pages/students/[org_id].vue
+++ pages/students/[org_id].vue
@@ -11,8 +11,10 @@ definePageMeta({
 })
 
 const userOrganizationsStore = useUserOrganizationsStore();
+const userStore = useUserStore();
 const userPermissionStore = useUserPermissionsStore();
 const studentStore = useStudentStore();
+const stripeStore = useStripeStore();
 const route = useRoute();
 
 await useAsyncData('userOrganizations', async () => {
@@ -25,6 +27,18 @@ await useAsyncData('permissions', async () => {
   if (!userOrganizationsStore.selectedOrganization) return false;
   await userPermissionStore.loadPermissions(userOrganizationsStore.selectedOrganization.role);
   return true;
+});
+
+await useAsyncData('student', async () => {
+    if(!userStore.user) return false;
+    await studentStore.loadStudent(route.params.org_id as string, userStore.user.id);
+    return true;
+});
+
+await useAsyncData('stripe', async () => {
+    await stripeStore.fetchStripeAccount();
+    await stripeStore.getStripeAppSettings();
+    return true;
 });
 </script>
 
