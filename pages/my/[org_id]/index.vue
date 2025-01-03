fa4736fcd9d73245f22fee5402f822058b21a2ab
stephaneBalelos <stephanedondyas2@gmail.com>
2025-01-03 12:33:40 +0100

    Lint fixes

--- pages/my/[org_id]/index.vue
+++ pages/my/[org_id]/index.vue
@@ -3,7 +3,7 @@
         <UDashboardNavbar title="Dashboard" badge="5" />
         <UDashboardPanelContent>
             <div class="grid grid-cols-2">
-                <OverviewAppointmentsList></OverviewAppointmentsList>
+                <OverviewAppointmentsList />
             </div>
         </UDashboardPanelContent>
     </UDashboardPanel>
