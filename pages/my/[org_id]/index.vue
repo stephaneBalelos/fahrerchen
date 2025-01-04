5ac9996ee223fae66bc8ced2b9bd9d8adda9fdc1
stephaneBalelos <stephanedondyas2@gmail.com>
2025-01-04 20:59:28 +0100

    Bill Overview

--- pages/my/[org_id]/index.vue
+++ pages/my/[org_id]/index.vue
@@ -2,8 +2,9 @@
     <UDashboardPanel grow>
         <UDashboardNavbar title="Dashboard" badge="5" />
         <UDashboardPanelContent>
-            <div class="grid grid-cols-2">
+            <div class="grid grid-cols-2 gap-4">
                 <OverviewAppointmentsList />
+                <OverviewBillList />
             </div>
         </UDashboardPanelContent>
     </UDashboardPanel>
