4c23a00c1908b2cbef9dc25524ef2b8dbfb8ac89
stephaneBalelos <stephanedondyas2@gmail.com>
2025-02-03 23:47:28 +0100

    Organization Card translation

--- components/ui/Cards/OrganizationCard.vue
+++ components/ui/Cards/OrganizationCard.vue
@@ -1,54 +1,72 @@
 <template>
-    <UPageCard
-        v-if="organization"
-      :key="organization.id"
-      class="mb-4"
-      :title="organization.name"
-      :description="organization.inserted_at"
-      :ui="{ wrapper: 'relative group org-card' }"
+  <UPageCard
+    v-if="organization"
+    :key="organization.id"
+    class="mb-4 cursor-pointer"
+    :title="organization.name"
+    :ui="{ wrapper: 'relative group org-card' }"
     @click="navigateTo(`/my/${organization.id}`)"
-    >
-    <template #icon>
-        <UAvatar
-            :src="organization.avatar"
-            :alt="organization.name"
-            size="lg"
-        />
+  >
+    <template #description>
+      {{ t("created_at", { date: formatDate(organization.inserted_at) }) }}
     </template>
-    </UPageCard>
+    <template #icon>
+      <UAvatar :src="organization.avatar" :alt="organization.name" size="lg" />
+    </template>
+  </UPageCard>
 </template>
 
 <script setup lang="ts">
-import type { Database } from '~/types/app.types';
+import type { Database } from "~/types/app.types";
+import { formatDate } from "~/utils/formatters";
 
 type Props = {
-    orgId: string
-}
-const props = defineProps<Props>()
-const supabase = useSupabaseClient<Database>()
+  orgId: string;
+};
+const props = defineProps<Props>();
+const supabase = useSupabaseClient<Database>();
 
-const config = useRuntimeConfig().public
+const config = useRuntimeConfig().public;
 
-const { data: organization } = useAsyncData(`organization_${props.orgId}`, async () => {
+const { t } = useI18n({
+  useScope: "local",
+});
+
+const { data: organization } = useAsyncData(
+  `organization_${props.orgId}`,
+  async () => {
     const { data, error } = await supabase
-        .from('organizations')
-        .select('*')
-        .eq('id', props.orgId).single()
+      .from("organizations")
+      .select("*")
+      .eq("id", props.orgId)
+      .single();
     if (error) {
-        throw error
+      throw error;
     }
-    console.log(data)
-    return data
-}, {
+    return data;
+  },
+  {
     transform: (data) => {
-        return {
-            ...data,
-            avatar: data.avatar_path ? `${config.supabase_storage_url}/object/public/organizations_avatars/${data.avatar_path}` : undefined
-        }
-    }
-})
+      return {
+        ...data,
+        avatar: data.avatar_path
+          ? `${config.supabase_storage_url}/object/public/organizations_avatars/${data.avatar_path}`
+          : undefined,
+      };
+    },
+  }
+);
 </script>
 
-<style scoped>
+<style scoped></style>
 
-</style>
+<i18n lang="json">
+{
+  "de": {
+    "created_at": "Erstellt am {date}"
+  },
+  "en": {
+    "created_at": "Created at {date}"
+  }
+}
+</i18n>
