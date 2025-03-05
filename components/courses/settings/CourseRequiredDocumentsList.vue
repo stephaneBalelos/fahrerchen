b0a798488a10ca8bf6b5c58a6794174d8473efb3
stephaneBalelos <stephanedondyas2@gmail.com>
2025-03-05 20:05:27 +0100

    Course Docs requirements

--- components/courses/settings/CourseRequiredDocumentsList.vue
+++ components/courses/settings/CourseRequiredDocumentsList.vue
@@ -1,92 +1,119 @@
 <template>
-    <UDashboardSection
-      :title="t('required_documents_settings_title')"
-      :description="t('required_documents_settings_desc')"
-      orientation="horizontal"
-      class="px-4 py-6"
-    >
-      <template #links>
-        <UButton
-          color="primary"
-          icon="i-heroicons-plus"
-          variant="soft"
-          size="2xs"
-          @click="openEditCourseRequiredDocument('')"
-          >{{ t('create_btn_label') }}</UButton
-        >
-      </template>
-      <UCard
-        :ui="{
-          body: {
-            base: 'divide-y divide-gray-200 dark:divide-gray-800 gap-4 flex flex-col',
-          },
-        }"
+  <UDashboardSection
+    :title="t('required_documents_settings_title')"
+    :description="t('required_documents_settings_desc')"
+    orientation="horizontal"
+    class="px-4 py-6"
+  >
+    <template #links>
+      <UButton
+        color="primary"
+        icon="i-heroicons-plus"
+        variant="soft"
+        size="2xs"
+        @click="openEditCourseRequiredDocument('')"
+        >{{ t("create_btn_label") }}</UButton
       >
-        <div
-          v-for="field in required_documents"
-          class="flex items-center justify-between pt-4 first:pt-0 gap-2"
-        >
-          <div class="flex flex-col gap-1 grow">
-            <p class="font-semibold">{{ field.name }}</p>
-            <span class="text-sm text-gray-500">{{ field.description }}</span>
-          </div>
-          <UButton
-            color="gray"
-            variant="solid"
-            @click="openEditCourseRequiredDocument(field.id)"
-            >{{ t('change') }}</UButton
-          >
+    </template>
+    <UCard
+      v-if="required_documents && required_documents.length > 0"
+      :ui="{
+        body: {
+          base: 'divide-y divide-gray-200 dark:divide-gray-800 gap-4 flex flex-col',
+        },
+      }"
+    >
+      <div
+        v-for="field in required_documents"
+        :key="field.id"
+        class="flex items-center justify-between pt-4 first:pt-0 gap-2"
+      >
+        <div class="flex flex-col gap-1 grow">
+          <p class="font-semibold">{{ field.name }}</p>
+          <span class="text-sm text-gray-500">{{ field.description }}</span>
         </div>
-      </UCard>
-    </UDashboardSection>
+        <UButton
+          color="gray"
+          variant="solid"
+          @click="openEditCourseRequiredDocument(field.id)"
+          >{{ t("change") }}</UButton
+        >
+      </div>
+    </UCard>
+    <UAlert
+      v-else
+      :title="t('no_documents')"
+      :description="t('no_documents_description')"
+    />
+  </UDashboardSection>
 </template>
 
 <script setup lang="ts">
-import type { Database } from '~/types/app.types';
-
+import type { Database } from "~/types/app.types";
+import EditCourseRequirementFrom from "~/components/forms/EditCourseRequirementFrom.vue";
 const { t } = useI18n({
-  useScope: 'local'
-})
-
+  useScope: "local",
+});
 
 const props = defineProps<{
   orgid: string;
   courseid: string;
 }>();
 
-const client = useSupabaseClient<Database>()
-
-  const { data:required_documents, error, status } = useAsyncData(`course_${props.courseid}_required_documents`, async () => {
-    const { data, error } = await client.from('course_required_documents').select('*').eq('course_id', props.courseid)
+const client = useSupabaseClient<Database>();
+const slideover = useSlideover();
+const { data: required_documents, refresh } = useAsyncData(
+  `course_${props.courseid}_required_documents`,
+  async () => {
+    const { data, error } = await client
+      .from("course_required_documents")
+      .select("*")
+      .eq("course_id", props.courseid);
     if (error) {
-      console.log(error)
-      throw error
+      console.log(error);
+      throw error;
     }
-    return data ?? []
-  })
+    return data ?? [];
+  }
+);
 
-function openEditCourseRequiredDocument(id: string) {
-  console.log(id)
+function openEditCourseRequiredDocument(id?: string) {
+  slideover.open(EditCourseRequirementFrom, {
+    courseid: props.courseid,
+    orgid: props.orgid,
+    requirementid: id,
+    "onRequirement-created": () => {
+      refresh();
+      slideover.close();
+    },
+    "onRequirement-saved": () => {
+      refresh();
+      slideover.close();
+    },
+  });
+
 }
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
 
 <i18n lang="json">
-  {
-    "de": {
-      "required_documents_settings_title": "Erforderliche Dokumente",
-      "required_documents_settings_desc": "Erforderliche Dokumente, die Student:innen hochladen müssen",
-      "create_btn_label": "Dokument hinzufügen",
-      "change": "Ändern"
-    },
-    "en": {
-      "required_documents_settings_title": "Required Documents",
-      "required_documents_settings_desc": "Theses document have to be uploaded by the student",
-      "create_btn_label": "Add document",
-      "change": "Edit"
-    }
+{
+  "de": {
+    "required_documents_settings_title": "Erforderliche Dokumente",
+    "required_documents_settings_desc": "Erforderliche Dokumente, die Student:innen hochladen müssen",
+    "create_btn_label": "Dokument hinzufügen",
+    "change": "Ändern",
+    "no_documents": "Keine Dokumente",
+    "no_documents_description": "Es wurden noch keine Dokumente Anforderungen festgelegt"
+  },
+  "en": {
+    "required_documents_settings_title": "Required Documents",
+    "required_documents_settings_desc": "Theses document have to be uploaded by the student",
+    "create_btn_label": "Add document",
+    "change": "Edit",
+    "no_documents": "No documents",
+    "no_documents_description": "No document requirements have been set yet"
   }
+}
 </i18n>
-
--- components/forms/EditCourseRequirementFrom.vue
+++ components/forms/EditCourseRequirementFrom.vue
@@ -5,7 +5,7 @@
       :state="state"
       :validate="validate"
       :validate-on="['submit']"
-      :onSubmit="saveCourseRequirement"
+      @submit="saveCourseRequirement"
     >
       <UDashboardSection
         title="Course Requirement"
@@ -20,37 +20,6 @@
           :ui="{ container: '' }"
         >
           <UInput v-model="state.name" autocomplete="on" size="md" />
-        </UFormGroup>
-
-        <UFormGroup
-          name="type"
-          label="Requirement Type"
-          description="Describe the type of requirement. Activity based on this requirement will be tracked."
-          required
-          class="grid grid-cols-1 gap-4 items-center"
-          :ui="{ container: '' }"
-        >
-          <USelectMenu
-            v-model="state.requirements_type"
-            value-attribute="id"
-            :options="requirements_types ?? []"
-            placeholder="Select a type"
-            option-attribute="type"
-            leadingIcon="i-heroicons-book-open"
-          />
-        </UFormGroup>
-
-        <UFormGroup v-if="selected_type" :ui="{ container: '' }">
-          <UAlert
-            :title="selected_type.type"
-            :description="selected_type.description"
-            color="primary"
-            variant="subtle"
-          >
-            <template #title="{ title }">
-              <span class="text-lg">Führerscheinklasse {{ title }}</span>
-            </template>
-          </UAlert>
         </UFormGroup>
 
         <UFormGroup
@@ -78,9 +47,8 @@
 </template>
 
 <script setup lang="ts">
-import { z } from "zod";
 import type { Database } from "~/types/app.types";
-import type { Form, FormSubmitEvent } from "#ui/types";
+import type { Form} from "#ui/types";
 
 type Props = {
   courseid: string;
@@ -89,10 +57,11 @@ type Props = {
 };
 
 type CourseRequirementEdit = Omit<
-  Database["public"]["Tables"]["course_requirements"]["Row"],
-  "id" | "organization_id" | "course_id"
+  Database["public"]["Tables"]["course_required_documents"]["Row"],
+  "id" | "organization_id" | "course_id" | "name_slug"
 >;
 
+const tutorialStore = useTutorialStore();
 const props = defineProps<Props>();
 const client = useSupabaseClient<Database>();
 const form = ref<Form<CourseRequirementEdit> | null>(null);
@@ -100,53 +69,28 @@ const $emit = defineEmits(['requirement-saved', 'requirement-created']);
 
 const {
   data: requirement,
-  error,
-  pending,
+
 } = useAsyncData(`course_requirement_${props.requirementid}`, async () => {
   if (!props.requirementid) return null;
-  const { data, error } = await client
-    .from("course_requirements")
+  const { data } = await client
+    .from("course_required_documents")
     .select("*")
     .eq("id", props.requirementid)
     .single();
   return data;
 });
 
-const {
-  data: requirements_types,
-  error: requirements_types_error,
-  pending: requirements_types_pending,
-} = useAsyncData("requirements_types", async () => {
-  const { data, error } = await client
-    .from("course_requirements_types")
-    .select("*");
-  return data;
-});
 
 const state = reactive<CourseRequirementEdit>({
   name: "",
-  description: "",
-  requirements_type: 1,
-  required: 1,
+  description: ""
 });
 
 onMounted(() => {
   if (requirement.value) {
     state.name = requirement.value.name;
     state.description = requirement.value.description;
-    state.requirements_type = requirement.value.requirements_type;
-    state.required = requirement.value.required;
   }
-});
-
-const selected_type = computed(() => {
-  if (!state.requirements_type) return null;
-  if (!requirements_types.value) return null;
-  return (
-    requirements_types.value.find(
-      (type) => type.id === state.requirements_type
-    ) ?? null
-  );
 });
 
 const validate = (state: CourseRequirementEdit) => {
@@ -162,8 +106,8 @@ const saveCourseRequirement = async () => {
     
   if (props.requirementid) {
     try {
-      const {data, error} = await client
-        .from("course_requirements")
+      const {error} = await client
+        .from("course_required_documents")
         .update(state)
         .eq("id", props.requirementid);
         if (error) throw error;
@@ -178,13 +122,14 @@ const saveCourseRequirement = async () => {
 
 const createCourseRequirement = async () => {
     try {
-        const {data, error} = await client.from("course_requirements").insert({
+        const { error} = await client.from("course_required_documents").insert({
             ...state,
             organization_id: props.orgid,
             course_id: props.courseid,
         });
         if (error) throw error;
         $emit('requirement-created');
+        tutorialStore.completeStep('step-4')
     } catch (error) {
         console.error(error);
     }
